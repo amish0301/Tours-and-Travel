154cd4c67e7fedb621a1e681c2bfbb3d61084b9d
amish <pithvaamish76@gmail.com>
2024-04-21 01:06:00 +0530

    Improved nav

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import logo from '../assets/travel-logo.jpg'
-import { Link, NavLink } from 'react-router-dom';
+import React, { forwardRef, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { Dialog, Transition } from "@headlessui/react";
+import { MenuIcon as Menu, XIcon as XIcon } from "@heroicons/react/outline";
+import Logo from "../assets/travel-logo.jpg";
 
 const nav_links = [
   {
@@ -22,46 +24,88 @@ const nav_links = [
 ];
 
 
-const Header = () => {
+const Nav = () => {
+  const [isMobile, setIsMobile] = useState(false);
   const [pathname, setPathname] = useState('');
 
+  let MyDialogPanel = forwardRef(function (props, ref) {
+    return <Dialog.Panel className='md:hidden' ref={ref} {...props} />
+  })
+
   return (
-    <nav className='contianer py-2 mb-5'>
-      <div className='max-w-screen-xxl flex justify-between align-center flex-wrap items-center p-4'>
-        <a href="/home" className='flex items-center space-x-3'>
-          <img src={logo} alt="logo" className='h-8' />
+    <nav className="p-3 flex bg-white justify-between items-center">
+      <div className="flex items-center flex-shrink-0 mr-6">
+        <NavLink to="/home" className='flex items-center space-x-3'>
+          <img src={Logo} alt="logo" className='h-8 object-cover max-w-12 max-h-12' />
           <span className='self-center text-xl font-semibold whitespace-nowrap'>Travellers</span>
-        </a>
-        <ul className='font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white light:bg-gray-800 light:border-gray-700'>
-          {
-            nav_links.map((val,index) => (
-              <li key={val.path} onClick={() => setPathname(window.location.pathname)}>
-                <NavLink to={val.path} className={`block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 hover:to-blue-400 light:hover:text-white md:dark:hover:bg-transparent ${pathname === val.path ? 'text-blue-400' : ''}`}>{val.name}</NavLink>
-              </li>
-            ))
-          }
-        </ul>
-        <div className='md:flex-col space-x-2 font-medium md:order-2'>
-          <button type='button' className='secondary-btn'>
-            <Link to={'/login'}>Login</Link>
-          </button>
-          <button type='button' className='primary-btn'>
-            <Link to={'/signup'}>SignUp</Link>
-          </button>
-        </div>
-
-        {/* logic for mobile menu  */}
-        <div className='lg:hidden'>
-          <button id="menu-toggle" class="lg:hidden focus:outline-none">
-            <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
-              <path class="hidden" id="close-icon" fill-rule="evenodd" clip-rule="evenodd" d="M19.413 6.215a.737.737 0 010 1.048l-4.645 4.642 4.645 4.642a.737.737 0 11-1.048 1.048l-4.645-4.642-4.642 4.642a.737.737 0 01-1.048-1.048l4.642-4.642-4.642-4.645a.737.737 0 111.048-1.048l4.642 4.642 4.645-4.642a.737.737 0 111.048 0z" />
-              <path class="block" id="open-icon" fill-rule="evenodd" clip-rule="evenodd" d="M2.737 6.215a.737.737 0 011.048 0l4.65 4.642L13.085 6.22a.737.737 0 111.048 1.048l-5.793 5.79a.737.737 0 01-1.048 0L2.737 7.263a.737.737 0 010-1.048z" />
-            </svg>
-          </button>
-        </div>
+        </NavLink>
       </div>
-    </nav>
-  )
-}
 
-export default Header
+      {/* Mobile Menu - hamburger menu */}
+      <button className="md:hidden p-2" onClick={() => setIsMobile(!isMobile)}>
+        <Menu className="h-6 w-6 text-gray-600" aria-hidden="true" />
+      </button>
+
+      {/* all the links */}
+      <div className="hidden md:flex gap-5">
+        {nav_links.map(link => (
+          <NavLink key={link.path} to={link.path} onClick={() => setPathname(link.path)} className={`font-medium hover:text-blue-800 ${pathname === link.path ? 'text-blue-500' : 'text-gray-600'}`}>{link.name}</NavLink>
+        ))}
+      </div>
+
+      {/* buttons */}
+      <div className="hidden md:flex">
+        <button type="button" className="secondary-btn">
+          <Link to={'/login'}>Login</Link>
+        </button>
+        <button type="button" className="primary-btn">
+          <Link to={'/signup'}>Signup</Link>
+        </button>
+      </div>
+
+      {/* for Mobile menu */}
+
+      <Transition
+        as={Dialog}
+        show={isMobile}
+        onClose={() => setIsMobile(false)}
+      >
+        <Transition.Child
+          as={MyDialogPanel}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <div className={`fixed inset-0 p-3 bg-white ${isMobile ? 'block' : 'hidden'}`}>
+            <div className="flex justify-between items-center">
+              <NavLink to="/home" className='flex items-center space-x-3'>
+                <img src={Logo} alt="logo" className='h-8 object-cover max-w-12 max-h-12' />
+                <span className='self-center text-xl font-semibold whitespace-nowrap'>Travellers</span>
+              </NavLink>
+
+              <button className="md:hidden p-2" onClick={() => setIsMobile(!isMobile)}>
+                <XIcon className="h-6 w-6 text-gray-600" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="mt-6">
+              <ul>
+                {
+                  nav_links.map(link => (
+                    <NavLink key={link.path} to={link.path} className={`font-medium block rounded-lg hover:bg-red-500 hover:text-blue-200 m-3 p-3 ${pathname === link.path ? 'text-blue-500' : 'text-gray-600'}`}>{link.name}</NavLink>
+                  ))
+                }
+              </ul>
+            </div>
+          </div>
+        </Transition.Child>
+      </Transition>
+
+      
+    </nav >
+  );
+};
+
+export default Nav;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'display': ['Poppins', 'sans-serif'],
+        'body': ['Inter', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
