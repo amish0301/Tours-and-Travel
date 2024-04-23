9a55cbcdd8c5021d7cefa1a88dab546a984dbb6a
amish <pithvaamish76@gmail.com>
2024-04-23 17:58:35 +0530

    about updated

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit rerum doloribus reiciendis exercitationem culpa accusamus laudantium modi dolor veritatis qui cupiditate vitae nulla explicabo incidunt reprehenderit sapiente tempore, dolorum ipsum!
-      Sunt, saepe non tenetur fuga id sapiente est consequuntur repellat corporis animi quibusdam molestias. Modi cumque voluptate sit quidem iste, saepe voluptatem itaque rem quibusdam odit tempora molestiae ipsam ipsa?
+      about
     </div>
   )
 }
